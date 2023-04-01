a4983e8823245f2cc57a582661259a6c4c0dec30
Bharat-2000 <yadeep999@gmail.com>
2023-04-01 16:23:50 +0530

    part 1 complete drag-drop functionality remaining

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./styles.css";
+import { Todo } from '../model';
+import SingleTodo from './SingleTodo';
+
+
+type TodoListProps = {
+    todos: Todo[],
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>,
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos, setTodos }: TodoListProps) => {
+    return (
+        <div className="todos">
+            {
+                todos.map(todo => (
+                    <SingleTodo
+                        todo={todo}
+                        todos={todos}
+                        key={todo.id}
+                        setTodos={setTodos}
+                    />
+                ))
+            }
+        </div>
+    )
+}
+
+export default TodoList
